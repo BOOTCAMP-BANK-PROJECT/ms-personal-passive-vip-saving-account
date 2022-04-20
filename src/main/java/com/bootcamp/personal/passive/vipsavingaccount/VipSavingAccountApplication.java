c7c6e0a723df0199c875ed0f46611c2f30727b76
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-20 14:00:27 -0500

    scaffolding

---
+++ src/main/java/com/bootcamp/personal/passive/vipsavingaccount/VipSavingAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.passive.vipsavingaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipSavingAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipSavingAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/personal/passive/vipsavingaccount/VipSavingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.passive.vipsavingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipSavingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
