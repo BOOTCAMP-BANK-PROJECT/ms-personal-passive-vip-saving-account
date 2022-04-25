2a37f175ead00cbfa2229609482541b7504bab52
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-25 16:09:18 -0500

    scaffolding deploy ready

--- src/main/java/com/bootcamp/personal/passive/vipsavingaccount/VipSavingAccountApplication.java
+++ src/main/java/com/bootcamp/personal/passive/vipsavingaccount/VipSavingAccountApplication.java
@@ -1,10 +1,43 @@
 package com.bootcamp.personal.passive.vipsavingaccount;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
 
+import java.util.logging.Logger;
+
+@OpenAPIDefinition(info = @Info(
+		title = "OpenAPI Documentation",
+		version = "1.0.0",
+		description = "OpenAPI Vip Saving Account Application Documentation v1.0.0",
+		contact = @Contact(name = "Samuel Luis", email = "samuel@company.com", url = "https://www.linkedin.com/in/samuel14luis/"),
+		license = @License(name = "MIT License", url = "https://choosealicense.com/licenses/mit/")
+))
+@EnableEurekaClient
 @SpringBootApplication
-public class VipSavingAccountApplication {
+public class VipSavingAccountApplication implements CommandLineRunner {
+
+	private static final Logger logger = Logger.getLogger(VipSavingAccountApplication.class.toString());
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//logger.log(Level.INFO, env.getProperty("spring.application.name"));
+		logger.info("Java version: " + env.getProperty("java.version"));
+		logger.info("Application name: " + env.getProperty("spring.application.name"));
+		logger.info("Properties file upload status: " + env.getProperty("my-own-app.properties.status"));
+		logger.info("Swagger: http://localhost:" + env.getProperty("server.port") +"/" + env.getProperty("springdoc.swagger-ui.path"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VipSavingAccountApplication.class, args);
